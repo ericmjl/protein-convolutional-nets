739c3dcf2f7f5c7dd4cfb612a92f15392597be15
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-11 19:07:00 -0400

    updated rsync_exclude

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,3 @@
-rsync -avzP --update * ericmjl@rous:~/Documents/github/protein-convolutional-nets --exclude-from exclude.txt 
+rsync -avzP --update * ericmjl@rous:~/Documents/github/protein-convolutional-nets --exclude-from rsync_exclude.txt
+
+rsync -avzP --update ericmjl@rous:~/Documents/github/protein-convolutional-nets/* ./ --exclude-from rsync_exclude.txt
