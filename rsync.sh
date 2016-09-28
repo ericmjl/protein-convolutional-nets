0e20293556f2d107801a1c92908ba5f5a0adc99c
Eric J. Ma <ericmajinglong@gmail.com>
2016-09-27 20:05:41 -0400

    fixed rsync script

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
-rsync -avzP --update * ericmjl@rous:~/Documents/github/protein-convolutional-nets --exclude-from rsync_exclude.txt
+rsync -avzP --update * ericmjl@rous:~/github/protein-convolutional-nets --exclude-from rsync_exclude.txt
 
-rsync -avzP --update ericmjl@rous:~/Documents/github/protein-convolutional-nets/* ./ --exclude-from rsync_exclude.txt
+rsync -avzP --update ericmjl@rous:~/github/protein-convolutional-nets/* ./ --exclude-from rsync_exclude.txt
